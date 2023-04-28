64f29455677ba7be3b5f7a696613ca2dd92c6e04
tantality <angel.tretyak@mail.ru>
2023-04-28 22:07:29 +0300

    feat: implement a redirect from '/' to '/vacancies'

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
+/* eslint-disable require-await */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/vacancies',
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
