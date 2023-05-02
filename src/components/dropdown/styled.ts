94c21791df73872da8b43175d7cc56f191cc4e84
tantality <angel.tretyak@mail.ru>
2023-05-02 19:00:58 +0300

    feat: add styles for dropdown according to the design system

--- src/components/dropdown/styled.ts
+++ src/components/dropdown/styled.ts
@@ -1,4 +1,59 @@
 import styled from '@emotion/styled';
 import { Select } from '@mantine/core';
+import { Inter } from 'next/font/google';
 
-export const StyledDropdown = styled(Select)``;
+const inter = Inter({ subsets: ['latin'] });
+
+export const StyledDropdown = styled(Select)`
+  .mantine-Select-wrapper {
+    &:hover {
+      .mantine-Select-input {
+        border-color: #5e96fc;
+      }
+    }
+  }
+
+  [aria-expanded='true'] {
+    svg {
+      color: #5e96fc !important;
+    }
+  }
+
+  .mantine-Select-dropdown,
+  .mantine-Select-input,
+  .mantine-Select-item {
+    border-radius: 8px;
+  }
+
+  .mantine-Select-input,
+  .mantine-Select-item {
+    font-family: ${inter.style.fontFamily};
+    font-style: ${inter.style.fontStyle};
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 20px;
+  }
+
+  .mantine-Select-input {
+    padding: 11px 42px 11px 12px;
+  }
+
+  .mantine-Select-item {
+    padding: 12px 8px;
+  }
+
+  [data-hovered] {
+    background-color: #deecff;
+  }
+
+  [data-selected] {
+    background-color: #5e96fc;
+    color: white;
+    font-weight: 500;
+    padding: 12px;
+
+    &:hover {
+      background-color: #5e96fc;
+    }
+  }
+`;
