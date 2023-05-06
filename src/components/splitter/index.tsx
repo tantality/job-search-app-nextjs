d9dc20cb9637d61e111a86339f715eb65e91714f
tantality <angel.tretyak@mail.ru>
2023-05-06 19:05:02 +0300

    feat: add a splitter component

---
+++ src/components/splitter/index.tsx
@@ -0,0 +1,6 @@
+import { FC } from 'react';
+import { StyledSplitter } from './styled';
+
+export const Splitter: FC = () => {
+  return <StyledSplitter />;
+};
---
+++ src/components/splitter/styled.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const StyledSplitter = styled.div`
+  border-radius: 50%;
+  height: 5px;
+  width: 5px;
+  background-color: #7b7c88;
+  margin: 0 1px;
+`;
