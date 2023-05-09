776db4594ce4c2581acc04ff95fb392096138a34
tantality <angel.tretyak@mail.ru>
2023-05-09 16:21:34 +0300

    refactor: apply constants

--- src/components/heading/styled.ts
+++ src/components/heading/styled.ts
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
 import { Title } from '@mantine/core';
 import { Inter } from 'next/font/google';
+import { HEADING_ORDER } from '@/constants';
 import { HeadingProps } from '.';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -11,9 +12,9 @@ export const StyledHeading = styled(Title)<Pick<HeadingProps, 'order'>>`
 
   ${({ order }) => {
   switch (order) {
-  case 1:
+  case HEADING_ORDER.H1:
     return 'font-size:28px; line-height: 34px';
-  case 2:
+  case HEADING_ORDER.H2:
     return 'font-size:20px; line-height: 24px';
   }
 }}
