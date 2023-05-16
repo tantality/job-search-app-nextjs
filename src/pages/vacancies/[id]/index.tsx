64cf798929bf02b1b2b9cc328d05246d2821bb4a
tantality <angel.tretyak@mail.ru>
2023-05-16 23:55:29 +0300

    feat: display the data of a particular vacancy on the page

--- src/pages/vacancies/[id]/index.tsx
+++ src/pages/vacancies/[id]/index.tsx
@@ -1,7 +1,32 @@
 import { useRouter } from 'next/router';
+import { Loader } from '@mantine/core';
+import { Container } from '@/components/container';
+import { useVacancy } from '@/hooks/useVacancy';
+import { VacancyCard } from '@/components/vacancy-card';
+import { SIZE } from '@/constants';
+import { VacancyDescription } from '@/components/vacancy-description';
+import { StyledContainer } from './styled';
 
 export default function VacancyPage() {
   const { query } = useRouter();
+  const vacancyId = Number(query['id']);
+  const isValidVacancyId = Boolean(Number(vacancyId));
 
-  return <p>{query['id']}</p>;
+  const { data: vacancy, isFetching, isSuccess } = useVacancy(vacancyId, { enabled: isValidVacancyId });
+
+  return (
+    <Container maxWidth={773}>
+      <main>
+        <StyledContainer>
+          {isSuccess && !isFetching ?
+            <>
+              <VacancyCard size={SIZE.LG} vacancy={vacancy} />
+              <VacancyDescription description={vacancy.vacancyRichText} />
+            </> :
+            <Loader />
+          }
+        </StyledContainer>
+      </main>
+    </Container>
+  );
 }
---
+++ src/pages/vacancies/[id]/styled.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const StyledContainer = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  row-gap: 20px;
+  justify-content: center;
+  align-items: center;
+`;
