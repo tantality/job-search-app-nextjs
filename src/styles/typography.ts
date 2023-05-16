ae860a4174574f6ce3fcc6bdc5c73ac41c77870c
tantality <angel.tretyak@mail.ru>
2023-05-16 22:20:01 +0300

    feat: add typography styles

---
+++ src/styles/typography.ts
@@ -0,0 +1,34 @@
+import { TypographyStylesProvider } from '@mantine/core';
+import { Inter } from 'next/font/google';
+import styled from 'styled-components';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const StyledTypography = styled(TypographyStylesProvider)`
+  font-family: ${inter.style.fontFamily};
+  font-style: ${inter.style.fontStyle};
+  font-weight: 400;
+
+  b {
+    font-size: 20px;
+    font-weight: 700;
+  }
+
+  p {
+    margin-bottom: 20px;
+  }
+
+  p:has(b) {
+    margin-bottom: 16px;
+  }
+
+  ul,
+  ol {
+    padding-left: 25px;
+    list-style: disc;
+
+    li {
+      margin-top: 0;
+    }
+  }
+`;
