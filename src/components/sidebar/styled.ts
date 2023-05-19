fd20f5ed8e15db2e037dac1366d7b856ec9a1c2a
tantality <angel.tretyak@mail.ru>
2023-05-19 20:00:49 +0300

    feat: adapt the sidebar component

--- src/components/sidebar/styled.ts
+++ src/components/sidebar/styled.ts
@@ -4,6 +4,22 @@ export const StyledSidebar = styled.aside`
   background-color: #ffffff;
   border-radius: 12px;
   border: 1px solid #eaebed;
+
+  @media (max-width: 800px) {
+    margin: 0 15%;
+  }
+
+  @media (max-width: 650px) {
+    margin: 0 10%;
+  }
+
+  @media (max-width: 650px) {
+    margin: 0 5%;
+  }
+
+  @media (max-width: 500px) {
+    margin: 0;
+  }
 `;
 
 export const StyledContainer = styled.div`
