00598257e58ae327c6f18173af34f92818da5730
tantality <angel.tretyak@mail.ru>
2023-05-19 19:09:51 +0300

    feat: adapt the two-column-layout component

--- src/components/layouts/two-column-layout/styled.ts
+++ src/components/layouts/two-column-layout/styled.ts
@@ -2,8 +2,13 @@ import styled from 'styled-components';
 
 export const StyledTwoColumnLayout = styled.div`
   display: grid;
-  grid-template-columns: minmax(200px, 315px) 1fr;
+  grid-template-columns: minmax(280px, 315px) minmax(400px, 1fr);
   column-gap: 28px;
   height: 100%;
   align-items: start;
+
+  @media (max-width: 800px) {
+    grid-template-columns: 1fr;
+    row-gap: 16px;
+  }
 `;
