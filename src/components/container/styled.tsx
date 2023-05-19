2e02f9ec90c3fe335a0d54187f8f6d10b2e4787d
tantality <angel.tretyak@mail.ru>
2023-05-19 18:54:06 +0300

    feat: adapt the container component

--- src/components/container/styled.tsx
+++ src/components/container/styled.tsx
@@ -8,4 +8,22 @@ export const StyledContainer = styled.div<Pick<ContainerProps, 'maxWidth'>>`
   width: 100%;
   height: 100%;
   padding: 40px 0 44px;
+
+  @media (max-width: 1180px) {
+    padding-left: 40px;
+    padding-right: 40px;
+  }
+
+  @media (max-width: 950px) {
+    padding-left: 30px;
+    padding-right: 30px;
+  }
+
+  @media (max-width: 550px) {
+    padding: 30px 20px 34px;
+  }
+
+  @media (max-width: 420px) {
+    padding: 30px 12px 34px;
+  }
 `;
