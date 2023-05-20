c6e1cb473a26e8410d678027ed7f9799b14c63fd
tantality <angel.tretyak@mail.ru>
2023-05-21 02:51:45 +0300

    feat: improve the adaptability of the content of the vacancies page

--- src/styles/pages/vacancies/index-styled.ts
+++ src/styles/pages/vacancies/index-styled.ts
@@ -16,27 +16,7 @@ export const StyledMainContent = styled.main`
     grid-template-rows: minmax(580px, 1fr) min-content;
   }
 
-  @media (max-width: 700px) {
-    grid-template-rows: minmax(600px, 1fr) min-content;
-  }
-
-  @media (max-width: 600px) {
-    grid-template-rows: minmax(620px, 1fr) min-content;
-  }
-
-  @media (max-width: 500px) {
-    grid-template-rows: minmax(640px, 1fr) min-content;
-  }
-
-  @media (max-width: 450px) {
-    grid-template-rows: minmax(720px, 1fr) min-content;
-  }
-
-  @media (max-width: 400px) {
-    grid-template-rows: minmax(740px, 1fr) min-content;
-  }
-
-  @media (max-width: 360px) {
-    grid-template-rows: minmax(780px, 1fr) min-content;
+  @media (max-width: 768px) {
+    grid-template-rows: 1fr min-content;
   }
 `;
