f8783a0ef356ea7e38263f5a8b6b0719f5845c28
tantality <angel.tretyak@mail.ru>
2023-05-20 23:53:59 +0300

    feat: adapt the message component

--- src/components/message/styled.tsx
+++ src/components/message/styled.tsx
@@ -7,8 +7,22 @@ export const StyledVacanciesEmptyState = styled.div`
   row-gap: 32px;
   justify-content: center;
   align-items: center;
+
+  @media (max-width: 900px) {
+    row-gap: 27px;
+  }
 `;
 
 export const StyledImage = styled(Image)`
   width: max-content;
+
+  @media (max-width: 700px) {
+    width: 220px;
+    height: 210px;
+  }
+
+  @media (max-width: 450px) {
+    width: 190px;
+    height: 180px;
+  }
 `;
