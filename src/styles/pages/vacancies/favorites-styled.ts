24c2f314e71d3288402223bb6df6e5812ca68d90
tantality <angel.tretyak@mail.ru>
2023-05-21 03:20:05 +0300

    feat: adapt the favorites page container

--- src/styles/pages/vacancies/favorites-styled.ts
+++ src/styles/pages/vacancies/favorites-styled.ts
@@ -6,4 +6,14 @@ export const StyledContainer = styled.div`
   justify-items: center;
   grid-template-rows: minmax(596px, 1fr) min-content;
   align-items: center;
+
+  @media (max-width: 680px) {
+    row-gap: 74px;
+    grid-template-rows: minmax(650px, 1fr) min-content;
+  }
+
+  @media (max-width: 640px) {
+    row-gap: 30px;
+    grid-template-rows: 1fr;
+  }
 `;
