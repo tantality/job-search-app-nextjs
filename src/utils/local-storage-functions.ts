da784955cd2e75dbe09fbe7770763552d23fcd80
tantality <angel.tretyak@mail.ru>
2023-05-22 23:22:26 +0300

    feat: set more precise types to functions

--- src/utils/local-storage-functions.ts
+++ src/utils/local-storage-functions.ts
@@ -1,8 +1,8 @@
-export const setItemToLocalStorage = (key: string, value: any): void => {
+export const setItemToLocalStorage = (key: string, value: string): void => {
   localStorage.setItem(key, value);
 };
 
-export const getItemFromLocalStorage = (key: string): any => {
+export const getItemFromLocalStorage = (key: string): string | null => {
   const value = localStorage.getItem(key);
   return value;
 };
