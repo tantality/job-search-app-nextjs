598d170780f346f927f00543479c7561e904dc2d
tantality <angel.tretyak@mail.ru>
2023-05-23 00:18:33 +0300

    feat: implement adding a token to a request

--- src/services/super-job/axios.ts
+++ src/services/super-job/axios.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { LOCAL_STORAGE_KEY } from '@/constants';
+import { getItemFromLocalStorage } from '@/utils';
 
 export const axiosInstance = axios.create({
   baseURL: 'https://startup-summer-proxy-production.up.railway.app/2.0/',
@@ -7,4 +9,17 @@ export const axiosInstance = axios.create({
     'x-secret-key': process.env.NEXT_PUBLIC_SECRET_KEY,
     'X-Api-App-Id': process.env.NEXT_PUBLIC_API_APP_SECRET_KEY,
   },
+});
+
+const pathsWithoutAuth = ['oauth2/password/', 'oauth2/refresh_token/', 'catalogues/'];
+
+axiosInstance.interceptors.request.use((config) => {
+  const accessToken = getItemFromLocalStorage(LOCAL_STORAGE_KEY.ACCESS_TOKEN);
+  const url = config.url as string;
+
+  if (!pathsWithoutAuth.includes(url)) {
+    config.headers['Authorization'] = `Bearer ${accessToken}`;
+  }
+
+  return config;
 });
