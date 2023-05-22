6a69a08096d486fb73b749dedb819d32a013af70
tantality <angel.tretyak@mail.ru>
2023-05-22 21:54:44 +0300

    feat: add props regulating the value of the number-input component

--- src/components/number-input/index.tsx
+++ src/components/number-input/index.tsx
@@ -8,6 +8,25 @@ interface NumberInputProps {
   onChange: (value: number | '') => void;
 }
 
+const DEFAULT_MAX_DELAY_IN_MS = 25;
+
 export const NumberInput: FC<NumberInputProps> = ({ placeholder, ...props }) => {
-  return <StyledNumberInput placeholder={placeholder ?? ''} size={SIZE.MD} type="number" min={1} {...props} />;
+  const calculateDelay = (stepCount: number): number => {
+    const delay = 1000 / stepCount ** 2;
+    return Math.max(delay, DEFAULT_MAX_DELAY_IN_MS);
+  };
+
+  return (
+    <StyledNumberInput
+      placeholder={placeholder ?? ''}
+      size={SIZE.MD}
+      type="number"
+      min={0}
+      startValue={500}
+      stepHoldDelay={500}
+      step={500}
+      stepHoldInterval={(stepCount): number => calculateDelay(stepCount)}
+      {...props}
+    />
+  );
 };
